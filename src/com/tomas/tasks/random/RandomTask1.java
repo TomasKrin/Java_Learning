ff42dbf52d3bf2efa23f7cf87da267f7f0d4c5e9
TomasKrin <tomas.krinickas2@gmail.com>
2026-04-17 14:12:44 +0300

    Task: Random Number Guess Result

---
+++ src/com/tomas/tasks/random/RandomTask1.java
@@ -0,0 +1,39 @@
+package com.tomas.tasks.random;
+
+import com.tomas.tasks.TaskProcessor;
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomTask1 implements TaskProcessor {
+  @Override
+  public void run() {
+    int userGuess;
+    int randomNumber;
+
+    Scanner scanner = new Scanner(System.in);
+    Random random = new Random();
+
+    randomNumber = random.nextInt(1, 11);
+
+    try {
+      System.out.print("Guess the number (1-10): ");
+      userGuess = scanner.nextInt();
+
+      System.out.println("Random number: " + randomNumber);
+      System.out.println("Your guess: " + userGuess);
+
+      if (randomNumber == userGuess) {
+        System.out.println("Exact match! You win!");
+      } else if (Math.abs(randomNumber - userGuess) == 1) {
+        System.out.println("Close!");
+      } else {
+        System.out.println("Wrong guess");
+      }
+
+    } catch (Exception e) {
+      System.out.println("Invalid input");
+    }
+
+    scanner.close();
+  }
+}
