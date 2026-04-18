db1646bb74dfde5c1d879696cb8689337b21517a
TomasKrin <tomas.krinickas2@gmail.com>
2026-04-18 20:00:46 +0300

    String Task 5: Reverse Words but Keep Order

---
+++ src/com/tomas/tasks/strings/StringsTask5.java
@@ -0,0 +1,33 @@
+package com.tomas.tasks.strings;
+
+import com.tomas.tasks.TaskProcessor;
+
+import java.util.Scanner;
+
+public class StringsTask5 implements TaskProcessor {
+	@Override
+	public void run () {
+		Scanner scanner = new Scanner(System.in);
+		StringBuilder stringBuilder = new StringBuilder();
+
+		String sentence;
+		String[] sentenceSplitArr;
+
+		System.out.print("Enter a sentence: ");
+		sentence = scanner.nextLine().trim();
+		sentenceSplitArr = sentence.split(" ");
+
+		String[] tempSentenceArray = new String[sentenceSplitArr.length];
+		
+		for (int i = sentenceSplitArr.length - 1; i >= 0; i--) {
+			stringBuilder.setLength(0);
+			String word = sentenceSplitArr[i];
+			String reversedWord = stringBuilder.append(word).reverse().toString();
+			tempSentenceArray[i] = reversedWord;
+		}
+
+		System.out.println(String.join(" " , tempSentenceArray));
+
+		scanner.close();
+	}
+}
