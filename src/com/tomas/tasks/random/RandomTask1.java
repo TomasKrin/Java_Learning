5b01cedde0482c6f63551633109d2a444c5426b0
TomasKrin <tomas.krinickas2@gmail.com>
2026-04-18 14:38:40 +0300

    added a check for user input value to be in range, deleted the formatter plugin

--- src/com/tomas/tasks/random/RandomTask1.java
+++ src/com/tomas/tasks/random/RandomTask1.java
@@ -1,39 +1,46 @@
 package com.tomas.tasks.random;
 
 import com.tomas.tasks.TaskProcessor;
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class RandomTask1 implements TaskProcessor {
-  @Override
-  public void run() {
-    int userGuess;
-    int randomNumber;
+	@Override
+	public void run () {
+		int userGuess;
+		int randomNumber;
 
-    Scanner scanner = new Scanner(System.in);
-    Random random = new Random();
+		Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
 
-    randomNumber = random.nextInt(1, 11);
+		randomNumber = random.nextInt(1 , 11);
 
-    try {
-      System.out.print("Guess the number (1-10): ");
-      userGuess = scanner.nextInt();
+		try {
+			System.out.print("Guess the number (1-10): ");
+			userGuess = scanner.nextInt();
 
-      System.out.println("Random number: " + randomNumber);
-      System.out.println("Your guess: " + userGuess);
+			if (userGuess >= 1 && userGuess <= 10) {
+				System.out.println("Random number: " + randomNumber);
+				System.out.println("Your guess: " + userGuess);
 
-      if (randomNumber == userGuess) {
-        System.out.println("Exact match! You win!");
-      } else if (Math.abs(randomNumber - userGuess) == 1) {
-        System.out.println("Close!");
-      } else {
-        System.out.println("Wrong guess");
-      }
+				if (randomNumber == userGuess) {
+					System.out.println("Exact match! You win!");
+				} else if (Math.abs(randomNumber - userGuess) == 1) {
+					System.out.println("Close!");
+				} else {
+					System.out.println("Wrong guess");
+				}
 
-    } catch (Exception e) {
-      System.out.println("Invalid input");
-    }
+			} else {
+				System.out.println("The value you entered is outside of range 1-10.");
+			}
 
-    scanner.close();
-  }
+		} catch (Exception e) {
+			System.out.println("Invalid input");
+
+		} finally {
+			scanner.close();
+		}
+	}
 }
