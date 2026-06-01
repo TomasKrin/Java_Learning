84dcb602bc65961ed918d3dca75a9dfb91350822
TomasKrin <tomas.krinickas2@gmail.com>
2026-06-01 14:17:14 +0300

    ValueAndReference Task 2: Passing Object References (Mutation vs Reassignment)

---
+++ src/com/tomas/tasks/valueAndReferenceTask/ValueAndReferenceTask2.java
@@ -0,0 +1,25 @@
+package com.tomas.tasks.valueAndReferenceTask;
+
+import com.tomas.tasks.TaskProcessor;
+
+public class ValueAndReferenceTask2 implements TaskProcessor {
+	@Override
+	public void run(){
+		StringBuilder sb = new StringBuilder("Hi");
+
+		changeText(sb);
+		System.out.println(sb);
+
+		reassign(sb);
+		System.out.println(sb);
+
+	}
+
+	void changeText(StringBuilder b){
+		b.append("!");
+	}
+
+	void reassign(StringBuilder b){
+		b = new StringBuilder("Bye");
+	}
+}
